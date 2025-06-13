72bd50b918c41b969b30a0a1b58a3b8f7a29fd5c
Christian Wiegand <christianwgd@users.noreply.github.com>
2025-06-13 19:43:04 +0200

    Add pagination to word list

--- word/views.py
+++ word/views.py
@@ -6,6 +6,7 @@ from word.models import Word
 
 class WordListView(LoginRequiredMixin, ListView):
     model = Word
+    paginate_by = 12
 
     def get_queryset(self):
         return Word.objects.order_by('?')
