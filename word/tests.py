e57c4ce07005da2bb36dc92fe6570168d6818ca5
Christian Wiegand <christianwgd@users.noreply.github.com>
2025-06-16 18:12:05 +0200

    Add word form tests

--- word/tests.py
+++ word/tests.py
@@ -1,11 +1,16 @@
+from django.contrib import auth
 from django.test import TestCase
 from django.utils.translation import gettext as _
 from faker import Faker
 
+from word.forms import WordForm
 from word.models import Word, Language
 
 
-class TestWordModel(TestCase):
+User = auth.get_user_model()
+
+
+class WordBaseTest(TestCase):
 
     def setUp(self):
         self.fake = Faker('de_DE')
@@ -24,9 +29,38 @@ class TestWordModel(TestCase):
             to_lang=self.language_to,
         )
 
+
+class TestWordModel(WordBaseTest):
+
+    def setUp(self):
+        super().setUp()
+
     def test_string_representation(self):
         self.assertEqual(str(self.word), self.word.source)
 
     def test_string_representation_language(self):
         self.assertEqual(str(self.language_from), self.language_from.name)
         self.assertEqual(str(self.language_to), self.language_to.name)
+
+
+class TestWordForms(WordBaseTest):
+
+    def setUp(self):
+        super().setUp()
+        self.user = User.objects.create_user(
+            username=self.fake.user_name(),
+        )
+
+    def test_word_form_valid(self):
+        data = {
+            'source': self.fake.word(),
+            'translation': self.fake.word(),
+        }
+        form = WordForm(data=data, user=self.user)
+        self.assertTrue(form.is_valid())
+
+    def test_word_form_invalid(self):
+        data = {}
+        form = WordForm(data=data, user=self.user)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors),2)
